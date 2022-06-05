bceabe38bf09135a5246edc7b1df49c966bececb
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-06-05 22:59:11 +0200

    Initial Commit

---
+++ MediaCatalog/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MediaCatalog
+//
+//  Created by Aitor Zubizarreta on 5/6/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
